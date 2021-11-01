d527cb48e49e82ce4e5e2fa4df073c7668ba1d50
Love Okum <l.okum@semicolon.africa>
2021-11-01 13:37:51 +0100

    updateing todo list project

---
+++ models.py
@@ -0,0 +1,15 @@
+from enum import Enum
+from datetime import datetime
+
+
+class Status(Enum):
+    UNCOMPLETED = "Uncompleted"
+    COMPLETED = "Completed"
+
+class Todo(db.Model):
+    __tablename__ = "todo"
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(), nullable=False)
+    status = db.Column(db.Enum(Status), nullable=False,
+                       default=Status.UNCOMPLETED)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.now())
